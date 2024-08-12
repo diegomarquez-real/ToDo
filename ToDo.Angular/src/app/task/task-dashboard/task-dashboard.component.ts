bfc6ea2a753db60cf9f2df5050638597f621e4d9
Diego Marquez <diegom@geoowl.com>
2024-08-12 10:14:17 -0700

    passed instnace of tasks instead of the parent component to limit data exposed to child component.

--- ToDo.Angular/src/app/task/task-dashboard/task-dashboard.component.ts
+++ ToDo.Angular/src/app/task/task-dashboard/task-dashboard.component.ts
@@ -9,8 +9,7 @@ declare const bootstrap: any;
 })
 export class TaskDashboardComponent {
   @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef;
-
-  tasks: Array<TaskLineItem> = [{ description: 'test task...' }];
+  tasks: Array<TaskLineItem> = [];
 
   removeTask(index: number) {
     this.tasks.splice(index, 1);
@@ -19,12 +18,12 @@ export class TaskDashboardComponent {
   addTask() {
     this.container.clear();
     let createTaskComponent = this.container.createComponent(CreateTaskComponent);
-    createTaskComponent.instance.taskDashboardComponent = this;
+    createTaskComponent.instance.tasks = this.tasks;
     const createTaskModal = new bootstrap.Modal(document.getElementById('create-task-container'), null)
     createTaskModal.show();
   }
 }
 
-class TaskLineItem {
+export class TaskLineItem {
   description: string = '';
 }
