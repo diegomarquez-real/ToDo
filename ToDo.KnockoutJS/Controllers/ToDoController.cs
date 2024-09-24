b283ca6ac00a8301357a63f5f7c24bb161286aca
Diego Marquez <diegom@geoowl.com>
2024-09-24 09:14:41 -0700

    initial todocontroller and ko functionality

---
+++ ToDo.KnockoutJS/Controllers/ToDoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo.KnockoutJS.Controllers
+{
+    public class ToDoController : Controller
+    { 
+        private readonly string[] _toDos = new string[] { "To Do 1", "To Do 2", "To Do 3" };
+
+        public ToDoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public JsonResult GetAll()
+        {
+            return Json(_toDos);
+        }
+    }
+}
---
+++ ToDo.KnockoutJS/wwwroot/js/ViewModels/ToDoViewModel.js
@@ -0,0 +1,35 @@
+﻿var ToDoViewModel = {
+    toDos: ko.observableArray([])
+};
+
+$(function () {
+    $.ajax({
+        url: '/ToDo/GetAll',
+        type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        beforeSend: function () {
+            console.log('Sending request...');
+        },
+        success: function (response) {
+            LoadToDos(response);
+            ko.applyBindings(ToDoViewModel);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log('Error');
+        },
+        complete: function () {
+            console.log('Request completed.');
+        }
+    });
+
+    function LoadToDos(items) {
+        for (var i = 0; i < items.length; i++) {
+            let item = {
+                id: i + 1,
+                description: items[i]
+            };
+            ToDoViewModel.toDos.push(item);
+        }
+    }
+});
