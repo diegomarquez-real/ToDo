dd234dc235bafebda27d7ef99cdaddecdff99dad
Diego Marquez <diegom@geoowl.com>
2024-09-25 15:22:04 -0700

    creation of task complete!

---
+++ ToDo.KnockoutJS/wwwroot/js/ViewModels/AddEditTaskViewModel.js
@@ -0,0 +1,31 @@
+﻿var AddEditTaskViewModel = {
+    description: ko.observable(),
+    saveTask: function () {
+        var createTaskModelJSON = {
+            Description: this.description()
+        };
+
+        $.ajax({
+            url: '/Task/Create',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(createTaskModelJSON),
+            beforeSend: function () {
+                console.log('Sending request...');
+            },
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log('Error');
+            },
+            complete: function () {
+                console.log('Request completed.');
+            }
+        });
+    }
+};
+
+$(function () {
+    ko.applyBindings(AddEditTaskViewModel, $('#addedit-task-modal')[0]);
+});
